ac5879efeb89b092823be2ece24622196413bbd2
astarryknight <whiteh4tter@gmail.com>
2024-07-02 14:09:04 -0400

    Initial Commit

---
+++ bt/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  bt
+//
+//  Created by Me on 7/2/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
